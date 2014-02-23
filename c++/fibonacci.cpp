f58798362b94556f4972fca4728ee99bdca7976d
Reinaldo Antonio Camargo Rauch <reinaldorauch@gmail.com>
2014-02-23 20:58:47 -0300

    Finalizado classe para geração da sequência de fibonacci

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -2,26 +2,80 @@
 
 class Fibonacci {
 
+    private:
+
+        double *buffer;
+
+        int bufferLenth;
+
+        void insertIntoBuffer(int i, double val) {
+            if(i <= this->bufferLenth) {
+                *(this->buffer + i) = val;
+                return;
+            } else {
+
+                size_t nextSize = (i + 1) * sizeof(double);
+
+                double *temp = (double*) realloc(this->buffer, nextSize);
+                if(temp == NULL)
+                    throw 1;
+                else
+                    this->buffer = temp;
+            }
+
+            *(this->buffer + i) = val;
+
+            this->bufferLenth = i;
+        }
+
     public:
 
         const static int FIRST_TERM = 0;
 
         const static int SECOND_TERM = 1;
 
-        Fibonacci () {}
+        Fibonacci () {
+            this->buffer = (double*) calloc(1, sizeof(double));
+            this->bufferLenth = 1;
+        }
 
-        int getNthTerm(int term) {
+        ~Fibonacci() {
+            free(this->buffer);
+        }
 
-            int sum, one = Fibonacci::FIRST_TERM, two = Fibonacci::SECOND_TERM;
+        double getNthTerm(int term) {
 
-            for(int i = 1; i < term; i++) {
-                sum = one + two;
+            if(term <= this->bufferLenth && *(this->buffer + term) != 0)
+                return *(this->buffer + term);
+
+            double sum = 0, one = Fibonacci::FIRST_TERM, two = Fibonacci::SECOND_TERM;
+
+            this->insertIntoBuffer(0, one);
+
+            sum = one + two;
+
+            this->insertIntoBuffer(1, sum);
+
+            int i;
+
+            for(i = 2; i < term; i++) {
+                this->insertIntoBuffer(i, sum);
                 one = two;
                 two = sum;
+                sum = one + two;
             }
+
+            this->insertIntoBuffer(i, sum);
 
             return sum;
 
+        }
+
+        void printBuffer() {
+            for (int i = 0; i <= this->bufferLenth; i++)
+            {
+                std::cout << "i = " << (i) << ", val = " << *(this->buffer + i) << '\n';
+            }
         }
 
 };
--- c++/main.cpp
+++ c++/main.cpp
@@ -14,5 +14,7 @@ int main() {
 
     std::cout << "O número gerado é: " << fb.getNthTerm(index) << '\n';
 
+    fb.printBuffer();
+
     return 0;
 }
